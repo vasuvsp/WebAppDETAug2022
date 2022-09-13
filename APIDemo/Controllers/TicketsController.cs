2d151d9fa4fef8003d7cfb1e041511c604d956a0
Vasu.Patil <vasuspatil555@gmail.com>
2022-09-13 12:47:56 +0530

    add new message to console

---
+++ APIDemo/Controllers/TicketsController.cs
@@ -0,0 +1,26 @@
+﻿using APIDemo.Models;
+using APIDemo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("all")]
+        public List<Ticket> Get()
+        {
+            return TicketService.GetAll();
+        }
+        [HttpGet]
+        [Route("tkets /{id}")]
+        public Ticket Get(int id)
+        {
+            Ticket t = TicketService.Get(id);
+            return t;
+        }
+    }
+}
---
+++ APIDemo/Services/TicketSerive.cs
@@ -0,0 +1,31 @@
+﻿using APIDemo.Models;
+
+namespace APIDemo.Services
+{
+    public static class TicketService
+    {
+        static List<Ticket> Tickets { get; }
+        static int nextId = 3;
+        static TicketService()
+        {
+            Tickets = new List<Ticket>
+                {
+                    new Ticket { ID = 1, BookedFor= "PlatinumPlus", Price=2000,Qty=20 },
+                    new Ticket { ID = 11, BookedFor= "Platinum", Price=789,Qty=56},
+                    new Ticket { ID = 12, BookedFor= "Gold", Price=500,Qty=27 },
+                    new Ticket { ID = 14, BookedFor= "Silver", Price=6000,Qty=89 },
+                  new Ticket { ID = 20, BookedFor= "general", Price=200,Qty=30 },
+                  new Ticket { ID = 31, BookedFor= "Classic Italian", Price=789,Qty=79 }
+                };
+        }
+
+        public static List<Ticket> GetAll() => Tickets;
+
+        public static Ticket Get(int id)
+        {
+            Ticket ticket = Tickets.FirstOrDefault(x => x.ID == id);
+            return ticket;
+
+        }
+    }
+}
